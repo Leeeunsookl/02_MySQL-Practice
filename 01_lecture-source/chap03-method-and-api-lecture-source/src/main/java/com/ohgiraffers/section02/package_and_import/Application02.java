7db67dbfbdf887950eb552f5c24286ac3515b89a
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-02-07 12:58:28 +0900

    [수업자료 240207 메소드 복습]

--- 01_lecture-source/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
+++ 01_lecture-source/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
@@ -3,6 +3,9 @@ package com.ohgiraffers.section02.package_and_import;
 
 import com.ohgiraffers.section01.method.Calculator;
 
+import java.util.Scanner;
+
+
 public class Application02 {
 
     public static void main(String[] args) {
@@ -20,6 +23,8 @@ public class Application02 {
 
         /* 목차. 1. non-static method 의 경우 */
         Calculator calc = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
     }
 
 }
