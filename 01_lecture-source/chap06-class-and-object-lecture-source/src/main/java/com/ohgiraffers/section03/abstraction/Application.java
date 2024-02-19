4df107f148379dfa16551416f190aa19a687e934
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-02-19 11:41:53 +0900

    [수업 추상화]

--- 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction/Application.java
+++ 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction/Application.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section03.abstraction;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -101,6 +103,44 @@ public class Application {
         *   - 속성 : 시동상태, 현재 시속
         *   - 행위 : 시동을 걸어라, 앞으로 가라, 멈춰라, 시동을 꺼라
         *  */
+
+        /* 카레이서가 한 명 등장합니다. */
+        CarRacer racer = new CarRacer();
+
+        /* 입력 받은 메뉴에 따라 카레이서의 행동이 달라집니다. */
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("========카레이싱 프로그램========");
+            System.out.println("1. 시동 걸기");
+            System.out.println("2. 전진");
+            System.out.println("3. 정지");
+            System.out.println("4. 시동 끄기");
+            System.out.println("9. 프로그램 종료");
+
+            System.out.print("메뉴를 입력하세요 : ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1 : racer.startUp(); break;
+                case 2 : racer.stepAccelator(); break;
+                case 3 : racer.stepBreak(); break;
+                case 4 : racer.turnOff(); break;
+                case 9 :
+                    System.out.println("프로그램을 종료합니다.."); break;
+                default:
+                    System.out.println("잘못된 번호를 입력했습니다."); break;
+            }
+
+            if(no == 9) {
+
+                break;
+
+            }
+
+        }
+
 
     }
 
--- 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction/CarRacer.java
+++ 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction/CarRacer.java
@@ -9,4 +9,30 @@ public class CarRacer {
     *  */
     private final Car car = new Car();
 
+
+    public void startUp() {
+
+        car.startUp();
+
+    }
+
+    public void stepAccelator() {
+
+        car.go();
+
+    }
+
+    public void stepBreak() {
+
+        car.stop();
+
+    }
+
+    public void turnOff() {
+
+        car.turnOff();
+
+    }
+
+
 }
