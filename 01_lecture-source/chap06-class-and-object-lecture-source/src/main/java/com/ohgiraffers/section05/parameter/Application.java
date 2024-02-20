ba5d12e807f8f5b3ec6c9c09f047ac5aefe59ab7
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-02-20 12:13:46 +0900

    [수업 자료]

--- 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/Application.java
+++ 01_lecture-source/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/Application.java
@@ -41,6 +41,22 @@ public class Application {
         System.out.println("인자로 전달하는 값 : " + r1);
         pt.testParameterClassType(r1);
 
+        // 메소드 호출 후 값 변경 확인
+        System.out.println("변경 후 사각형의 넓이와 둘레===========");
+        r1.calcArea();
+        r1.calcRound();
+
+        /* 목차. 4. 클래스 자료형 배열은 뒤에서 다룰 예정이다. */
+
+        /* 목차. 5. 가변인자 */
+        /* 인자로 전달하는 값의 갯수가 정해지지 않은 경우 가변배열을 이용할 수 있다. */
+
+//        pt.testVariableLengthArrayParameter();    // 인자가 아무것도 없는 경우 에러 발생
+        pt.testVariableLengthArrayParameter("홍길동");     // 가변인자는 전달하지 않아도 된다.
+        pt.testVariableLengthArrayParameter("이순신", "볼링");   // 1개도 가능
+        pt.testVariableLengthArrayParameter("신사임당", "볼링", "축구", "배드민턴"); // 여러 개도 가능
+        pt.testVariableLengthArrayParameter("아무개" , new String[] {"테니스", "서예", "떡썰기"}); // 배열도 가능
+
     }
 
 }
