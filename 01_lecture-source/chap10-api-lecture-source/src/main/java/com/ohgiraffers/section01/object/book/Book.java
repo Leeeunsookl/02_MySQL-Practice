1ff4d4447e3c387bf7d50b4f9f3bb4eb3260e005
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-02-23 10:10:58 +0900

    [api 자료]

---
+++ 01_lecture-source/chap10-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/book/Book.java
@@ -0,0 +1,61 @@
+package com.ohgiraffers.section01.object.book;
+
+public class Book {
+
+    private int number;         // 책 번호
+    private String title;       // 책 제목
+    private String author;      // 책 저자
+    private int price;          // 책 가격
+
+    public Book() {}
+
+    public Book(int number, String title, String author, int price) {
+        this.number = number;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "number=" + number +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ 01_lecture-source/chap10-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/run/Application02.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.object.run;
+
+public class Application02 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. Object 클래스의 equals() 메소드 오버라이딩 목적을 이해하고, 활용할 수 있다. */
+        /* 필기.
+        *   equals() 메소드 오버라이딩
+        *   equals() 메소드는 매개변수로 전달받은 인스턴스와 == 연산하여 true or false 를 반환한다.
+        *   즉, 동일한 인스턴스인 지 비교하는 역할을 한다.
+        *  */
+
+    }
+
+}
